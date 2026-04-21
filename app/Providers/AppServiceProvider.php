30c3fc21afe6be6a2bd214f168459218d7682f44
Otávio Vicente <otaviovicente09@gamil.com>
2026-04-21 17:30:41 -0300

    Forçando HTTPS para evitar erro de segurança

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -21,5 +22,8 @@ class AppServiceProvider extends ServiceProvider
 {
     \Carbon\Carbon::setLocale('pt_BR');
     setlocale(LC_TIME, 'pt_BR', 'pt_BR.utf-8', 'portuguese');
+    if (config('app.env') === 'production') {
+        URL::forceScheme('https');
+    }
 }
 }
